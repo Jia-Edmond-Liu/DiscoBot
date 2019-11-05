2faedbbb703a51f914ffa45057b924c65da93b5c
Ed <jia.liu@uoit,net>
2019-11-04 19:02:00 -0500

    Added package.json & code to join server. Bot now connects and responds

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-//initial commit
+var auth = require('./tokens.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(auth.TOKEN);
