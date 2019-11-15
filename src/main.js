cea3260423c597dcf0a0251c9d2cfeaf53ae9692
Ed <jia.liu@uoit,net>
2019-11-15 14:23:08 -0500

    Bot youtube fixed. Now Functional

--- src/main.js
+++ src/main.js
@@ -3,19 +3,39 @@
 var auth = require('./json/tokens.json');
 var jokes = require('./json/jokes.json');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const youtubeBase = "https://www.youtube.com/watch?v=";
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+//returns a URL from youtube given an ID
+function getVideoURL(videoId){
+	return youtubeBase + videoId;
+}
 
 
 //Youtube get function
 function getYoutube(url){
+	var cheat;
 	var xHttpRequest = new XMLHttpRequest();
     xHttpRequest.onreadystatechange = function() {
         if (xHttpRequest.readyState == 4 && xHttpRequest.status == 200)
-			console.log(xHttpRequest.responseText);
-    }
-    xHttpRequest.open("GET", url); // true for asynchronous
+		/*for some reason, this returns a string in JSON format instead of
+		a JSON file and JSON.parse() returns me undefined when i try to access
+		any of its properties
+
+		As im only getting the top video, I can search for index of
+		"videoId" and "snippet" as both strings only appear once.
+		The subtraction is to account for the length of extra
+		chars infront of each string respectively.
+
+		TODO: CONVERT THE STRING INTO A JSON FILE OR FIND A BETTER WORK AROUND*/
+			var vidIndex = xHttpRequest.responseText.indexOf("videoId") + 11;
+			var snipIndex = xHttpRequest.responseText.indexOf("\"", vidIndex) - vidIndex;
+			cheat = getVideoURL(xHttpRequest.responseText.substr(vidIndex,snipIndex));
+	}
+    xHttpRequest.open("GET", url, false);
     xHttpRequest.send(null);
+	return cheat;
 }
 
 
@@ -37,12 +57,11 @@ client.on('message', msg => {
 //https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q="keyword here"&type=video&key="api key here"
 client.on('message', msg=>{
 	if(msg.content.substring(0,8).toUpperCase() == "-YOUTUBE"){
-		var substr = msg.content.substring(11,msg.content.length);
+		var substr = msg.content.substring(9,msg.content.length);
 		var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&" +
-			"maxResults=20&q=" + substr + "&type=video&key=" +
+			"maxResults=1&q=" + substr + "&type=video&key=" +
 			auth.YOUTUBE_API;
-		getYoutube(url);
-		console.log(url);
+		msg.channel.send(getYoutube(url));
 	}
 });
 
